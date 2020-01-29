03468d70648dc93c81c6d1d7452570627da3d515
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-01-29 13:52:58 +0100

    Fixed path to script

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const exec = require('child_process').exec
-exec('bash publish.sh', (error, stdout, stderr) => {
+const childProc = require('child_process')
+// test if we're in a GitHub Actions context so we can still test locally how the action is behaving
+const root = process.cwd().includes('/home/runner/work') ? '/home/runner/work/_actions/kaskadi/action-npmpub/master/' : './'
+const pathToScript = `${root}publish.sh`
+
+childProc.exec(pathToScript, (err, stdout, stderr) => {
   console.log(stdout)
-  if (error !== null) {
+  if (err !== null) {
     console.log(stderr)
   }
 })
