332a6c8f618b1f9c1f13790ed33639181ad217d7
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-02-26 16:46:31 +0100

    Addec control flow for local testing

--- publish.sh
+++ publish.sh
@@ -9,10 +9,13 @@ REMOTE_VERSION="$(npm view $PACKAGE_NAME version)"
 if [ "$CURRENT_VERSION" == "$REMOTE_VERSION" ]
 then
   npm --no-git-tag-version version $UPDATE_TYPE
-  CURRENT_VERSION="$(getVersion)"
-  git config --global user.name $INPUT_USERNAME
-  git config --global user.email $INPUT_EMAIL
-  git commit -am "Upgraded to $CURRENT_VERSION"
-  git push
+  if [ "$GITHUB_ACTIONS" == "true" ]
+  then
+    CURRENT_VERSION="$(getVersion)"
+    git config --global user.name $INPUT_USERNAME
+    git config --global user.email $INPUT_EMAIL
+    git commit -am "Upgraded to $CURRENT_VERSION"
+    git push
+  fi
 fi
 npm publish --access public
