3df183ca902517bdb8ff2c8b070c5017ade560e1
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-08-24 12:54:42 +0200

    Moved publish script to src

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 const childProc = require('child_process')
 // test if we're in a GitHub Actions context so we can still test locally how the action is behaving
 const root = process.env.GITHUB_ACTIONS && process.env.GITHUB_REPOSITORY !== 'kaskadi/action-npmpub' ? '/home/runner/work/_actions/kaskadi/action-npmpub/master/' : `${process.cwd()}/`
-const pathToScript = `${root}publish.sh`
+const pathToScript = `${root}src/publish.sh`
 
 const tags = ['*patch*', '*minor*', '*major*']
 const commitMsgTag = process.env.COMMIT_MSG.slice(0, 7)
