2538edf154f6c9ce1e02b950be8b9b329bfaf387
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-12-03 13:25:17 +0100

    Refactored action logic to not use bash script

---
+++ src/helpers/publish.js
@@ -0,0 +1,76 @@
+const { spawnSync } = require('child_process')
+const { existsSync, readFileSync } = require('fs')
+
+module.exports = (core, updateType) => {
+  const { version, npmVersion } = getVersions(core)
+  if (version === npmVersion) {
+    upgradeVersion(core, updateType)
+    pushChanges(core)
+  }
+  publish(core)
+}
+
+function getPjson () {
+  return JSON.parse(readFileSync('package.json', 'utf8'))
+}
+
+function getVersions ({ startGroup, endGroup, error }) {
+  startGroup('Comparing local and remote versions of package')
+  console.log('INFO: retrieving local and remote versions of package...')
+  if (!existsSync('package.json')) {
+    const errorMsg = 'This repository does not seem to be an NPM package'
+    error(errorMsg)
+    throw new Error(`${errorMsg}, aborting...`)
+  }
+  const { name, version } = getPjson()
+  const npmVersion = spawnSync('npm', ['view', name, 'version']).stdout.toString().trim().replace('v', '')
+  console.log('SUCCESS: local and remote versions retrieved!')
+  endGroup()
+  return { version, npmVersion }
+}
+
+function upgradeVersion ({ startGroup, endGroup }, updateType) {
+  startGroup('Upgrading package version')
+  console.log(`INFO: upgrading ${updateType} version of package...`)
+  spawnSync('npm', ['--no-git-tag-version', 'version', updateType], { stdio: 'inherit' })
+  console.log(`SUCCESS: ${updateType} version of package successfully upgraded!`)
+  endGroup()
+}
+
+function pushChanges ({ startGroup, endGroup, getInput }) {
+  const repo = process.env.GITHUB_REPOSITORY
+  startGroup(`Pushing new package.json and package-lock.json to ${repo}`)
+  if (process.env.GITHUB_ACTIONS) {
+    console.log('INFO: configurating git user based on action inputs...')
+    const username = getInput('username', { required: true })
+    const email = getInput('email', { required: true })
+    spawnSync('git', ['config', '--global', 'user.name', username], { stdio: 'inherit' })
+    spawnSync('git', ['config', '--global', 'user.email', email], { stdio: 'inherit' })
+    console.log(`SUCCESS: git user ${username} with email ${email} successfully configurated!`)
+  }
+  console.log('INFO: staging package.json and package-lock.json files...')
+  cliOp('git', ['add', 'package.json'])
+  cliOp('git', ['add', 'package-lock.json'])
+  console.log('SUCCESS: files successfully staged!')
+  console.log('INFO: commiting new files...')
+  const { version } = getPjson()
+  cliOp('git', ['commit', '-m', `Upgraded to ${version}`])
+  console.log('SUCCESS: files successfully commited!')
+  console.log(`INFO: pushing new files to ${repo}`)
+  cliOp('git', ['push'])
+  console.log('SUCCESS: files successfully pushed!')
+  endGroup()
+}
+
+function publish ({ startGroup, endGroup }) {
+  startGroup('Publishing package to NPM')
+  cliOp('npm', ['publish', '--access', 'public'])
+  endGroup()
+}
+
+function cliOp (cli, args) {
+  if (process.env.TEST_ENV) {
+    args = [...args.slice(0, 1), '--dry-run', ...args.slice(1)]
+  }
+  return spawnSync(cli, args, { stdio: 'inherit' })
+}
--- src/main.js
+++ src/main.js
@@ -1,36 +1,6 @@
-const { spawn } = require('child_process')
-const { startGroup, endGroup } = require('@actions/core')
-const path = require('path')
+const getUpdateType = require('./helpers/get-update-type.js')
+const publish = require('./helpers/publish.js')
+const core = require('@actions/core')
 
-const pathToScript = path.join(__dirname, 'publish.sh')
-
-function main () {
-  startGroup('Determining update type')
-  process.env.UPDATE_TYPE = getUpdateType(process.env.COMMIT_MSG)
-  endGroup()
-
-  const proc = spawn('bash', [pathToScript])
-  proc.stdout.on('data', log)
-  proc.stderr.on('data', log)
-  proc.on('error', console.log)
-  proc.on('exit', code => {
-    if (code !== 0) {
-      throw new Error('An error occured while trying to publish your package...')
-    }
-  })
-}
-
-function getUpdateType (commitMsg) {
-  const tags = ['*patch*', '*minor*', '*major*']
-  console.log('INFO: determining update type from commit message pattern...')
-  const commitMsgTag = commitMsg ? commitMsg.slice(0, 7) : '*patch*'
-  const updateType = tags.includes(commitMsgTag) ? commitMsgTag.replace(new RegExp(/\*/, 'g'), '') : 'patch'
-  console.log(`SUCCESS: update type looks to be ${updateType}`)
-  return updateType
-}
-
-function log (data) {
-  console.log(data.toString().trim())
-}
-
-main()
+const updateType = getUpdateType(core, process.env.COMMIT_MSG)
+publish(core, updateType)
