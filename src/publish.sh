ab059e69e9bd4272a6f5aa1fd5c11346b83d2955
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-12-03 11:55:35 +0100

    Added --dry-run flag + simplified conditional flow

--- src/publish.sh
+++ src/publish.sh
@@ -4,7 +4,10 @@ getVersion() {
   node -e "console.log(require('./package.json').version)"
 }
 getRemoteVersion() {
-  npm view $1 version
+  # set flag for not exiting shell on error
+  set -e
+  npm view $1 version || "1.0.0"
+  set +e
 }
 getName() {
   node -e "console.log(require('./package.json').name)"
@@ -14,6 +17,14 @@ startLogGroup() {
 }
 endLogGroup() {
   echo "::endgroup::"
+}
+cliOp () {
+  if [ "$TEST_ENV" == "true" ]
+  then
+    $1 $2 --dry-run "${@:3}"
+  else
+    $1 $2 "${@:3}"
+  fi 
 }
 # compare current and remote version
 startLogGroup "Comparing local and remote versions of package"
@@ -30,27 +41,27 @@ then
   npm --no-git-tag-version version $UPDATE_TYPE
   echo "SUCCESS: $UPDATE_TYPE version of package successfully upgraded!"
   endLogGroup
+  startLogGroup "Pushing new package.json and package-lock.json to $GITHUB_REPOSITORY"
   if [ "$GITHUB_ACTIONS" == "true" ]
   then
-    startLogGroup "Pushing new package.json and package-lock.json to $GITHUB_REPOSITORY"
     echo "INFO: configurating git user based on action inputs..."
     git config --global user.name $INPUT_USERNAME
     git config --global user.email $INPUT_EMAIL
     echo "SUCCESS: git user $INPUT_USERNAME with email $INPUT_EMAIL successfully configurated!"
-    echo "INFO: staging package.json and package-lock.json files..."
-    git add package.json
-    git add package-lock.json
-    echo "SUCCESS: files successfully staged!"
-    echo "INFO: commiting new files..."
-    CURRENT_VERSION="$(getVersion)"
-    git commit -m "Upgraded to $CURRENT_VERSION"
-    echo "SUCCESS: files successfully commited!"
-    echo "INFO: pushing new files to $GITHUB_REPOSITORY"
-    git push
-    echo "SUCCESS: files successfully pushed!"
-    endLogGroup
   fi
+  echo "INFO: staging package.json and package-lock.json files..."
+  cliOp git add package.json
+  cliOp git add package-lock.json
+  echo "SUCCESS: files successfully staged!"
+  echo "INFO: commiting new files..."
+  CURRENT_VERSION="$(getVersion)"
+  cliOp git commit -m "Upgraded to $CURRENT_VERSION"
+  echo "SUCCESS: files successfully commited!"
+  echo "INFO: pushing new files to $GITHUB_REPOSITORY"
+  cliOp git push
+  echo "SUCCESS: files successfully pushed!"
+  endLogGroup
 fi
 startLogGroup "Publishing package to NPM"
-npm publish --access public
+cliOp npm publish --access public
 endLogGroup
